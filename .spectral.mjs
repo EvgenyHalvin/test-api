94c1325a94c38ecc77048b7194519b4aa0a2634e
Евгений Жданов <eugenezhdanov1@gmail.com>
2023-03-11 10:53:53 +0300

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4OTY0Nw/spectral.js?branch_slug=main&token=5a1849bb-e2db-4ca4-b735-8a67227116a0";
+export default { extends: ruleset };
